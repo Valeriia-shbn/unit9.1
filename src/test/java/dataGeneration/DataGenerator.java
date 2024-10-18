a35021d25b2adc18f4888a8c78cbefbe9862c9af
Valeriia <valeriiashbn@gmail.com>
2024-10-18 08:11:32 -0700

    fix issues

--- src/test/java/dataGeneration/DataGenerator.java
+++ src/test/java/dataGeneration/DataGenerator.java
@@ -5,7 +5,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.Getter;
+
 
 import java.util.Locale;
 
